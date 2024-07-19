0b2a041c3504c470840cc48bc36c710a5be0a858
IB Teguh Teja Murti <teguh.teja@gmail.com>
2024-07-19 19:08:54 +0800

    17.02 : Create App and Edit manifest.py

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,21 +1,37 @@
 # -*- coding: utf-8 -*-
-# More info at https://www.odoo.com/documentation/master/reference/module.html
-
 {
-    "name": "Real Estate",
-    "depends": [
-        "base",
-        "web",
+    'name': "Estate",
+
+    'summary': 'Module for real estate advertisements',
+
+    'description': """
+Long description of module's purpose
+    """,
+
+    'author': "IB Teguh Teja M",
+    'website': "https://teguhteja.id",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Real Estate',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/views.xml',
+        # 'views/templates.xml',
     ],
-    "data": [
-        "security/ir.model.access.csv",
-        "views/estate_property_offer_views.xml",
-        "views/estate_property_tag_views.xml",
-        "views/estate_property_type_views.xml",
-        "views/estate_property_views.xml",
-        "views/res_users_views.xml",
-        "views/estate_menus.xml",
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
     ],
-    "application": True,
-    "license": "LGPL-3",
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
+
--- estate/models/__init__.py
+++ estate/models/__init__.py
@@ -1,5 +1,3 @@
-from . import estate_property
-from . import estate_property_offer
-from . import estate_property_tag
-from . import estate_property_type
-from . import res_users
+# -*- coding: utf-8 -*-
+
+from . import models
